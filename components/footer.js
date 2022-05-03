4241036bd822f3ca1dd6f677512600df432f642e
allistermugaisi <allistermugaisi@gmail.com>
2022-05-03 23:39:24 +0300

    created footer section

--- components/footer.js
+++ components/footer.js
@@ -2,9 +2,272 @@ import React from 'react';
 
 const Footer = () => {
 	return (
-		<div>
-			<h2>footer</h2>
-		</div>
+		<>
+			<footer className="bg-gray-50 py-8 sm:py-12">
+				<div className="max-w-6xl mx-auto px-4">
+					<div className="sm:flex sm:flex-wrap sm:-mx-4 md:py-4">
+						<div className="px-4 sm:w-1/2 md:w-1/4 xl:w-1/6">
+							<h5 className="text-base font-gray-600 font-semibold mb-6">
+								Products
+							</h5>
+							<ul className="list-none footer-links">
+								<li className="mb-2">
+									<a
+										href="#"
+										className="border-b border-solid border-transparent hover:border-indigo-600 hover:text-indigo-600"
+									>
+										Consultations
+									</a>
+								</li>
+								<li className="mb-2">
+									<a
+										href="#"
+										className="border-b border-solid border-transparent hover:border-indigo-600 hover:text-indigo-600"
+									>
+										Book Appointment
+									</a>
+								</li>
+								<li className="mb-2">
+									<a
+										href="#"
+										className="border-b border-solid border-transparent hover:border-indigo-600 hover:text-indigo-600"
+									>
+										Book Lab Test & Diagnostics
+									</a>
+								</li>
+								<li className="mb-2">
+									<a
+										href="#"
+										className="border-b border-solid border-transparent hover:border-indigo-600 hover:text-indigo-600"
+									>
+										Analytics
+									</a>
+								</li>
+								<li className="mb-2">
+									<a
+										href="#"
+										className="border-b border-solid border-transparent hover:border-indigo-600 hover:text-indigo-600"
+									>
+										Telehealth
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div className="px-4 sm:w-1/2 md:w-1/4 xl:w-1/6 mt-8 sm:mt-0">
+							<h5 className="text-base font-gray-600 font-semibold mb-6">
+								Use cases
+							</h5>
+							<ul className="list-none footer-links">
+								<li className="mb-2">
+									<a
+										href="#"
+										className="border-b border-solid border-transparent hover:border-indigo-600 hover:text-indigo-600"
+									>
+										Public Hospitals
+									</a>
+								</li>
+								<li className="mb-2">
+									<a
+										href="#"
+										className="border-b border-solid border-transparent hover:border-indigo-600 hover:text-indigo-600"
+									>
+										Private Hospitals
+									</a>
+								</li>
+								<li className="mb-2">
+									<a
+										href="#"
+										className="border-b border-solid border-transparent hover:border-indigo-600 hover:text-indigo-600"
+									>
+										Medical Clinics
+									</a>
+								</li>
+								<li className="mb-2">
+									<a
+										href="#"
+										className="border-b border-solid border-transparent hover:border-indigo-600 hover:text-indigo-600"
+									>
+										Pharmacy
+									</a>
+								</li>
+								<li className="mb-2">
+									<a
+										href="#"
+										className="border-b border-solid border-transparent hover:border-indigo-600 hover:text-indigo-600"
+									>
+										Clinical Laboratories
+									</a>
+								</li>
+								<li className="mb-2">
+									<a
+										href="#"
+										className="border-b border-solid border-transparent hover:border-indigo-600 hover:text-indigo-600"
+									>
+										Home Care Services
+									</a>
+								</li>
+								<li className="mb-2">
+									<a
+										href="#"
+										className="border-b border-solid border-transparent hover:border-indigo-600 hover:text-indigo-600"
+									>
+										Ambulance Service Providers
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div className="px-4 sm:w-1/2 md:w-1/4 xl:w-1/6 mt-8 md:mt-0">
+							<h5 className="text-base font-gray-600 font-semibold mb-6">
+								Company
+							</h5>
+							<ul className="list-none footer-links">
+								<li className="mb-2">
+									<a
+										href="#"
+										className="border-b border-solid border-transparent hover:border-indigo-600 hover:text-indigo-600"
+									>
+										Customers
+									</a>
+								</li>
+								<li className="mb-2">
+									<a
+										href="#"
+										className="border-b border-solid border-transparent hover:border-indigo-600 hover:text-indigo-600"
+									>
+										Enterprise
+									</a>
+								</li>
+								<li className="mb-2">
+									<a
+										href="#"
+										className="border-b border-solid border-transparent hover:border-indigo-600 hover:text-indigo-600"
+									>
+										Partners
+									</a>
+								</li>
+								<li className="mb-2">
+									<a
+										href="#"
+										className="border-b border-solid border-transparent hover:border-indigo-600 hover:text-indigo-600"
+									>
+										Career
+									</a>
+								</li>
+								<li className="mb-2">
+									<a
+										href="#"
+										className="border-b border-solid border-transparent hover:border-indigo-600 hover:text-indigo-600"
+									>
+										Blogs
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div className="px-4 sm:w-1/2 md:w-1/4 xl:w-1/6 mt-8 md:mt-0">
+							<h5 className="text-base font-gray-600 font-semibold mb-6">
+								Help
+							</h5>
+							<ul className="list-none footer-links">
+								<li className="mb-2">
+									<a
+										href="#"
+										className="border-b border-solid border-transparent hover:border-indigo-600 hover:text-indigo-600"
+									>
+										Support
+									</a>
+								</li>
+								<li className="mb-2">
+									<a
+										href="#"
+										className="border-b border-solid border-transparent hover:border-indigo-600 hover:text-indigo-600"
+									>
+										Guides
+									</a>
+								</li>
+								<li className="mb-2">
+									<a
+										href="#"
+										className="border-b border-solid border-transparent hover:border-indigo-600 hover:text-indigo-600"
+									>
+										Community
+									</a>
+								</li>
+								<li className="mb-2">
+									<a
+										href="#"
+										className="border-b border-solid border-transparent hover:border-indigo-600 hover:text-indigo-600"
+									>
+										Help Center
+									</a>
+								</li>
+								<li className="mb-2">
+									<a
+										href="#"
+										className="border-b border-solid border-transparent hover:border-indigo-600 hover:text-indigo-600"
+									>
+										Contact Us
+									</a>
+								</li>
+								<li className="mb-2">
+									<a
+										href="#"
+										className="border-b border-solid border-transparent hover:border-indigo-600 hover:text-indigo-600"
+									>
+										Privacy & Terms
+									</a>
+								</li>
+								<li className="mb-2">
+									<a
+										href="#"
+										className="border-b border-solid border-transparent hover:border-indigo-600 hover:text-indigo-600"
+									>
+										COVID-19
+									</a>
+								</li>
+								<li className="mb-2">
+									<a
+										href="#"
+										className="border-b border-solid border-transparent hover:border-indigo-600 hover:text-indigo-600"
+									>
+										Sitemap
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div className="px-4 mt-4 sm:w-1/3 xl:w-1/6 sm:mx-auto xl:mt-0 xl:ml-auto">
+							<h5 className="text-base font-gray-600 font-semibold mb-6 sm:text-center xl:text-left">
+								Stay connected
+							</h5>
+							<div className="flex sm:justify-center xl:justify-start">
+								<a
+									href=""
+									className="w-8 h-8 border border-gray-400 rounded-full text-center py-1 text-gray-600 hover:text-white hover:bg-blue-600 hover:border-blue-600"
+								>
+									<i className="fab fa-facebook"></i>
+								</a>
+								<a
+									href=""
+									className="w-8 h-8 border border-gray-400 rounded-full text-center py-1 ml-2 text-gray-600 hover:text-white hover:bg-blue-400 hover:border-blue-400"
+								>
+									<i className="fab fa-twitter"></i>
+								</a>
+								<a
+									href=""
+									className="w-8 h-8 border border-gray-400 rounded-full text-center py-1 ml-2 text-gray-600 hover:text-white hover:bg-red-600 hover:border-red-600"
+								>
+									<i className="fab fa-google-plus-g"></i>
+								</a>
+							</div>
+							<div className="py-14">
+								<p className="text-sm">
+									&copy; {new Date().getFullYear()} MedSwift ltd
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</>
 	);
 };
 
--- components/navbar.js
+++ components/navbar.js
@@ -50,7 +50,7 @@ const Navbar = () => {
 						<nav className="hidden md:flex space-x-10">
 							<a
 								href="#"
-								className="text-base font-medium text-gray-500 hover:text-gray-900"
+								className="text-base font-medium text-gray-600 hover:text-gray-900"
 							>
 								Home
 							</a>
@@ -59,7 +59,7 @@ const Navbar = () => {
 								<button
 									type="button"
 									className="
-                   group bg-white rounded-md text-gray-500 inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none pb-8'
+                   group bg-white rounded-md text-gray-600 inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none pb-8'
                   "
 									onMouseEnter={() => (
 										setFlyerThree(true), setFlyer(false), setFlyerTwo(false)
@@ -369,7 +369,7 @@ const Navbar = () => {
 								<button
 									type="button"
 									className="
-                   group bg-white rounded-md text-gray-500 inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none pb-8'
+                   group bg-white rounded-md text-gray-600 inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none pb-8'
                   "
 									onMouseEnter={() => (
 										setFlyer(true), setFlyerTwo(false), setFlyerThree(false)
@@ -632,13 +632,13 @@ const Navbar = () => {
 
 							<a
 								href="#"
-								className="text-base font-medium text-gray-500 hover:text-gray-900"
+								className="text-base font-medium text-gray-600 hover:text-gray-900"
 							>
 								Pricing
 							</a>
 							<a
 								href="#"
-								className="text-base font-medium text-gray-500 hover:text-gray-900"
+								className="text-base font-medium text-gray-600 hover:text-gray-900"
 							>
 								About
 							</a>
@@ -646,7 +646,7 @@ const Navbar = () => {
 								{/* Item active: "text-gray-900", Item inactive: "text-gray-500" */}
 								<button
 									type="button"
-									className="group bg-white rounded-md text-gray-500 inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none"
+									className="group bg-white rounded-md text-gray-600 inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none"
 									onMouseEnter={() => (
 										setFlyerTwo(!flyerTwo),
 										setFlyer(false),
